fe442c4fe4c962ce5f8a5c170c40982c6f44d9a3
Emanuele <cannone.emanuele@gmail.com>
2021-03-17 15:20:39 +0100

    collegamento header-rotte ok

--- laravel-base-crud/resources/views/partials/header.blade.php
+++ laravel-base-crud/resources/views/partials/header.blade.php
@@ -1,3 +1,8 @@
 <header>
-    <h1>qui va l'header</h1>
+    <nav>
+        <ul>
+            <li><a href="{{ route ('home') }}">Home</a></li>
+            <li><a href="{{ route ('auto') }}">Auto</a></li>
+        </ul>
+    </nav>
 </header>
